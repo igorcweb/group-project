eaff3f3c3d225b5677d0d54760029844dd7147b8
Igor Chernyshev <igorchern07@gmail.com>
2018-07-19 01:36:50 -0500

    marker is nowchanging with input, map is centered

--- script.js
+++ script.js
@@ -7,18 +7,91 @@ var alert = document.querySelector('.alert');
 var display = document.querySelector('.display');
 var url;
 var city;
+var address;
+var marker;
+var map;
+var mapDisplay = document.querySelector('#map');
+// var infoContent;
+// var infowindow = new google.maps.InfoWindow({
+//   content: infoContent
+// });
+
+// map and marker function
+function initializeMap() {
+  mapDisplay.classList.remove('d-none');
+  var mapOptions = {
+    center: {
+      lat: parseFloat(lat),
+      lng: parseFloat(lng)
+    },
+    zoom: 5,
+    // disableDefaultUI: true,
+    zoomControl: true
+  };
+  //create map
+  map = new google.maps.Map(document.getElementById('map'), mapOptions);
+
+  // create marker
+  marker = new google.maps.Marker({
+    position: {
+      lat: parseFloat(lat),
+      lng: parseFloat(lng)
+    },
+    map: map,
+    draggable: true
+  });
+  //call info window up
+  // infowindow.open(map, marker);
+  // drag event
+}
 
 go.addEventListener('click', function(e) {
   e.preventDefault();
-  var letterSpaces = /^[a-zA-Z, ]+$/;
-
-  if (cityInput.value.match(letterSpaces)) {
-    city = cityInput.value.trim();
-    url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&APPID=${api_key}`;
+  address = cityInput.value.replace('.', '').trim();
+  console.log('address: ', address);
+  if (address) {
+    url = `http://api.openweathermap.org/data/2.5/weather?q=${address}&APPID=${api_key}`;
     getWeather();
+    var geocoder = new google.maps.Geocoder();
+    geocoder.geocode(
+      {
+        address: address
+      },
+      function(results, status) {
+        if (status === google.maps.GeocoderStatus.OK) {
+          lat = results[0].geometry.location.lat();
+          lng = results[0].geometry.location.lng();
+          // loadWeather();
+          // updateInputs();
+          // focusMarker();
+          // focusMap();
+          initializeMap();
+          focusMap();
+          focusMarker();
+          google.maps.event.addListener(marker, 'dragend', function() {
+            lat = marker.position.lat();
+            lng = marker.position.lng();
+            //loadWeather();
+            //updateInputs();
+            focusMap();
+          });
+        }
+      }
+    );
     cityInput.value = '';
   }
 });
+
+function focusMap() {
+  map.setCenter(marker.position);
+}
+
+function focusMarker() {
+  marker.setPosition({
+    lat: lat,
+    lng: lng
+  });
+}
 
 function celsius(tempKelvin) {
   var celsius = _.round(tempKelvin - 273.15);
@@ -44,7 +117,7 @@ function getWeather() {
       var output = document.createElement('div');
       output.setAttribute('class', 'text-center mt-2');
       output.innerHTML = `
-      <h4 class="text-center mt-2">Weather in ${city}:</h4> 
+      <h4 class="text-center mt-2">Weather in ${address}:</h4> 
       <p>Current Temperature: ${f} / ${c}</p>
       <p>Humidity: ${h}</p>
       `;
@@ -52,6 +125,7 @@ function getWeather() {
     })
     .catch(function(error) {
       console.log(error);
+      mapDisplay.classList.add('d-none');
       display.innerHTML = '';
       alert.classList.remove('d-none');
       setTimeout(function() {
