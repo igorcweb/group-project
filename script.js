13056ff6ab68e6ad2a23d8e8d5888ebe111f93ab
Igor Chernyshev <igorchern07@gmail.com>
2018-07-20 16:26:34 -0500

    able to display users's location on page load

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
-// Open weather API Key
+// Open Weather
 var ow_api_key = '7b371ff33bf7c8589f05eb50e8efe90c';
+//Google Maps
 var gm_api_key = 'AIzaSyATPSbFvHa14zbdf5HYoPBO4jCwteR8GfM';
 
-var cityInput = document.querySelector('#cityInput');
+var locInput = document.querySelector('#locInput');
 var go = document.querySelector('button[type=submit]');
 var alert = document.querySelector('.alert');
 var display = document.querySelector('#display');
-var url;
+var owUrl;
 var formattedAddress;
 var marker;
 var map;
@@ -15,6 +16,26 @@ var lng;
 var F = true; // Set degrees to Fahrenheit
 var localTime;
 var mapDisplay = document.querySelector('#map');
+
+//Get User's Location
+(function getlocation() {
+  var geoIpUrl = 'https://geoip-db.com/json/';
+  axios
+    .get(geoIpUrl)
+    .then(function(res) {
+      var data = res.data;
+      console.log(data);
+      address = `${data.city}, ${data.country_name}`;
+      lat = data.latitude;
+      lng = data.longitude;
+      initializeMap();
+      integrateGoogleMaps(address);
+      getWeather();
+    })
+    .catch(function(error) {
+      console.log(error);
+    });
+})();
 
 // map and marker function
 function initializeMap() {
@@ -45,56 +66,61 @@ function initializeMap() {
   // drag event
 }
 
+function integrateGoogleMaps(address) {
+  owUrl = `http://api.openweathermap.org/data/2.5/weather?q=${address}&APPID=${ow_api_key}`;
+  var geocoder = new google.maps.Geocoder();
+  geocoder.geocode(
+    {
+      address: address
+    },
+    function(results, status) {
+      if (status === google.maps.GeocoderStatus.OK) {
+        //some formatted addresses have numbers in them - this clears them out
+        formattedAddress = results[0].formatted_address.replace(/[0-9]/g, '');
+        lat = results[0].geometry.location.lat();
+        lng = results[0].geometry.location.lng();
+
+        //Get timezone
+        (function() {
+          var tzUrl = `https://maps.googleapis.com/maps/api/timezone/json?location=${lat},${lng}&timestamp=1458000000&key=${gm_api_key}`;
+          axios
+            .get(tzUrl)
+            .then(function(res) {
+              console.log('timezone: ', res.data.timeZoneId);
+              var timeZone = res.data.timeZoneId.toString();
+              console.log('timezone: ', timeZone);
+              localTime = moment.tz(timeZone).format('MMMM Do, h:mm a');
+              console.log('local time: ', localTime);
+            })
+            .catch(function(error) {
+              console.log(error);
+            });
+        })();
+        mapDisplay.classList.remove('d-none');
+
+        // updateInputs();
+        getWeather();
+        initializeMap();
+        focusMap();
+        focusMarker();
+        google.maps.event.addListener(marker, 'dragend', function() {
+          lat = marker.position.lat();
+          lng = marker.position.lng();
+          focusMap();
+        });
+      }
+    }
+  );
+}
+
 go.addEventListener('click', function(e) {
   e.preventDefault();
   var regex = /^[a-zA-Z,. ]+$/;
-  var address = cityInput.value.replace('.', '').trim();
+  var address = locInput.value.replace('.', '').trim();
   if (address && address.match(regex)) {
     console.log('address: ', address);
-    url = `http://api.openweathermap.org/data/2.5/weather?q=${address}&APPID=${ow_api_key}`;
-    var geocoder = new google.maps.Geocoder();
-    geocoder.geocode(
-      {
-        address: address
-      },
-      function(results, status) {
-        if (status === google.maps.GeocoderStatus.OK) {
-          //some formatted addresses have numbers in them - this clears them out
-          formattedAddress = results[0].formatted_address.replace(/[0-9]/g, '');
-          lat = results[0].geometry.location.lat();
-          lng = results[0].geometry.location.lng();
-          var tzUrl = `https://maps.googleapis.com/maps/api/timezone/json?location=${lat},${lng}&timestamp=1458000000&key=${gm_api_key}`;
-          function getTimeZone() {
-            axios
-              .get(tzUrl)
-              .then(function(res) {
-                console.log('timezone: ', res.data.timeZoneId);
-                var timeZone = res.data.timeZoneId.toString();
-                console.log('timezone: ', timeZone);
-                localTime = moment.tz(timeZone).format('MMMM Do, h:mm a');
-                console.log('local time: ', localTime);
-              })
-              .catch(function(error) {
-                console.log(error);
-              });
-          }
-          mapDisplay.classList.remove('d-none');
-          getTimeZone();
-
-          // updateInputs();
-          getWeather();
-          initializeMap();
-          focusMap();
-          focusMarker();
-          google.maps.event.addListener(marker, 'dragend', function() {
-            lat = marker.position.lat();
-            lng = marker.position.lng();
-            focusMap();
-          });
-        }
-      }
-    );
-    cityInput.value = '';
+    integrateGoogleMaps(address);
+    locInput.value = '';
   } else {
     valAlert();
   }
@@ -102,7 +128,7 @@ go.addEventListener('click', function(e) {
 
 function valAlert() {
   console.log('valAlert running');
-  cityInput.value = '';
+  locInput.value = '';
   display.innerHTML = '';
   mapDisplay.classList.add('d-none');
   alert.classList.remove('d-none');
@@ -134,7 +160,7 @@ function fahrenheit(tempKelvin) {
 
 function getWeather() {
   axios
-    .get(url)
+    .get(owUrl)
     .then(function(res) {
       display.innerHTML = '';
       var data = res.data;
@@ -166,7 +192,6 @@ function getWeather() {
 
 display.addEventListener('click', function(e) {
   if (e.target && e.target.classList.contains('fc')) {
-    console.log(e.target);
     F = !F;
     getWeather();
   }
