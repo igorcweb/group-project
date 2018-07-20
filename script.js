93fb1d5e56fed721ee5ab11894899b9b82f71e9a
Igor Chernyshev <igorchern07@gmail.com>
2018-07-20 02:46:22 -0500

    sanitized formatted location

--- script.js
+++ script.js
@@ -7,14 +7,13 @@ var go = document.querySelector('button[type=submit]');
 var alert = document.querySelector('.alert');
 var display = document.querySelector('#display');
 var url;
-var city;
-var address;
+var formattedAddress;
 var marker;
 var map;
 var lat;
 var lng;
 var F = true; // Set degrees to Fahrenheit
-var currentTime;
+var localTime;
 var mapDisplay = document.querySelector('#map');
 // var infoContent;
 // var infowindow = new google.maps.InfoWindow({
@@ -54,7 +53,7 @@ function initializeMap() {
 go.addEventListener('click', function(e) {
   e.preventDefault();
   var regex = /^[a-zA-Z,. ]+$/;
-  address = cityInput.value.replace('.', '').trim();
+  var address = cityInput.value.replace('.', '').trim();
   if (address && address.match(regex)) {
     console.log('address: ', address);
     url = `http://api.openweathermap.org/data/2.5/weather?q=${address}&APPID=${ow_api_key}`;
@@ -65,6 +64,8 @@ go.addEventListener('click', function(e) {
       },
       function(results, status) {
         if (status === google.maps.GeocoderStatus.OK) {
+          //some formatted addresses have numbers in them - this clears them out
+          formattedAddress = results[0].formatted_address.replace(/[0-9]/g, '');
           lat = results[0].geometry.location.lat();
           lng = results[0].geometry.location.lng();
           var tzUrl = `https://maps.googleapis.com/maps/api/timezone/json?location=${lat},${lng}&timestamp=1458000000&key=${gm_api_key}`;
@@ -75,13 +76,14 @@ go.addEventListener('click', function(e) {
                 console.log('timezone: ', res.data.timeZoneId);
                 var timeZone = res.data.timeZoneId.toString();
                 console.log('timezone: ', timeZone);
-                currentTime = moment.tz(timeZone).format('h:mm a');
-                console.log('current time: ', currentTime);
+                localTime = moment.tz(timeZone).format('MMMM Do, h:mm a');
+                console.log('local time: ', localTime);
               })
               .catch(function(error) {
                 console.log(error);
               });
           }
+          mapDisplay.classList.remove('d-none');
           getTimeZone();
 
           // updateInputs();
@@ -98,14 +100,15 @@ go.addEventListener('click', function(e) {
       }
     );
     cityInput.value = '';
-  } else if (address) {
-    validation();
+  } else {
+    valAlert();
   }
 });
 
-function validation() {
-  console.log('validation running');
+function valAlert() {
+  console.log('valAlert running');
   cityInput.value = '';
+  display.innerHTML = '';
   mapDisplay.classList.add('d-none');
   alert.classList.remove('d-none');
   setTimeout(function() {
@@ -146,20 +149,22 @@ function getWeather() {
       var tempMin = F
         ? fahrenheit(data.main.temp_min) + '°F'
         : celsius(data.main.temp_min) + '°C';
-      var cfButton = F ? '°C' : '°F';
+      var cfButton = F ? 'C' : 'F';
       console.log(data);
       var h = data.main.humidity + '%';
       var output = document.createElement('div');
       output.setAttribute('class', 'text-center mt-2');
       output.innerHTML = `
-      <h4 class="text-center mt-2">Weather in <span id="address">${address}</span>:</h4> 
+      <h4 class="text-center mt-2">${formattedAddress}</h4>
+      <h5 class="time text-center mb-2">Local Time - ${localTime}</h5>
+
       <p>Today's Temperature: ${tempMax} / ${tempMin} <span><button class="fc p-0 btn btn-primary">${cfButton}</button></span></p>
       <p>Humidity: ${h}</p>
       `;
       display.appendChild(output);
     })
     .catch(function(error) {
-      validation();
+      valAlert();
       console.log(error);
     });
 }
